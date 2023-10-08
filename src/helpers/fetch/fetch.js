68023e25b43bdab644eabe1281cd2d2c1e484c49
ilse31 <ilhamprazsetyo33@gmail.com>
2023-10-08 20:05:55 +0700

    add user agent

--- src/helpers/fetch/fetch.js
+++ src/helpers/fetch/fetch.js
@@ -2,7 +2,11 @@ const { default: axios } = require("axios");
 
 const fetchHtml = async (url) => {
   try {
-    const response = await axios.get(url);
+    const headers = {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+    };
+    const response = await axios.get(url, headers);
     if (response.status === 200) {
       return response.data;
     }
